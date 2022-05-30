3feba6aeb92c4931a8d6bcc3d760fe7cf93a4493
Joel Shin <joel@fluxxlabs.com>
2022-05-30 12:08:25 -0700

    error handler middleware

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,16 @@
+export class DatabaseConnectionError extends Error {
+  statusCode = 500
+  reason = "error connecting to database"
+
+  constructor() {
+    super()
+
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype)
+  }
+
+  serializeErrors() {
+    return [
+      { message: this.reason }
+    ]
+  }
+}
