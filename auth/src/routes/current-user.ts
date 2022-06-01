114e14f522f913d77ed88bfbf2d7c5543b41d7ed
Joel Shin <joel@fluxxlabs.com>
2022-06-01 11:31:40 -0700

    removed temp test of require-auth middleware on current-user route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,11 +1,9 @@
 import express from 'express'
-import { requireAuth } from '../middlewares/require-auth'
-
 import { currentUser } from '../middlewares/current-user'
 
 const router = express.Router()
 
-router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
+router.get('/api/users/currentuser', currentUser, (req, res) => {
   // send proper payload or null, not undefined
   res.send({ currentUser: req.currentUser || null })
 })
