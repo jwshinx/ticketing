2d4f8b0b3d750d4e230e017e632d97973c3a6937
Joel Shin <joel@fluxxlabs.com>
2022-06-01 11:41:24 -0700

    client: added coded to detect logged in or not

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,12 +3,11 @@ import BuildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
   console.log("+++> LandingPage 0 currentUser:", currentUser);
-
-  return (
-    <div>
-      <h1>Landing page 5</h1>
-    </div>
-  );
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  )
 };
 
 LandingPage.getInitialProps = async (context) => {
