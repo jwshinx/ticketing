47db67afef924ee1367c473cfe2979d63f6e68d1
Joel Shin <joel@fluxxlabs.com>
2022-06-01 12:18:51 -0700

    implmtd getInitialProps in _app and play nice with other pages; created header component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,13 +1,69 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import BuildClient from '../api/build-client';
+import Header from '../components/header'
 
-const AppComponent = ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
       <div className="container">
+        <Header currentUser={currentUser.email} />
         <Component {...pageProps} />
       </div>
     </div>
   );
 };
 
+AppComponent.getInitialProps = async appContext => {
+  const client = BuildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx, client, data.currentUser);
+  }
+
+  console.log('+++> _app getInitialProps', pageProps)
+  // data includes currentUser
+  return {
+    pageProps,
+    ...data
+  };
+};
+
 export default AppComponent;
+
+/*
+import 'bootstrap/dist/css/bootstrap.css';
+import BuildClient from '../api/build-client';
+import Header from '../components/header';
+
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <div className="container">
+        <Component currentUser={currentUser} {...pageProps} />
+      </div>
+    </div>
+  );
+};
+
+AppComponent.getInitialProps = async appContext => {
+  const client = BuildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx, client, data.currentUser);
+  }
+
+  // data includes currentUser
+  return {
+    pageProps,
+    ...data
+  };
+};
+
+export default AppComponent;
+
+*/
