edbf2de42bbca864ca5152325984a0def7589519
Joel Shin <joel@fluxxlabs.com>
2022-06-02 12:31:51 -0700

    tickets: create ticket and ticket model

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
 import { requireAuth, validateRequest } from '@jslamela/common'
 
+import { Ticket } from '../models/ticket'
+
 const router = express.Router()
 
 router.post(
@@ -12,8 +14,16 @@ router.post(
     body('price').isFloat({ gt: 0 }).withMessage('price must be greate than 0')
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-  res.sendStatus(200)
-})
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id
+    })
+    await ticket.save()
+    res.status(201).send(ticket)
+  }
+)
 
 export { router as createTicketRouter }
