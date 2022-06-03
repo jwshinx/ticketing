12eda0cf35298752877530763abcfe07e0217619
Joel Shin <joel@fluxxlabs.com>
2022-06-02 21:05:16 -0700

    tickets: update route

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -8,6 +8,7 @@ import { errorHandler, NotFoundError, currentUser } from '@jslamela/common'
 import { createTicketRouter } from './routes/new'
 import { showTicketRouter } from './routes/show'
 import { indexTicketRouter } from './routes/index'
+import { updateTicketRouter } from './routes/update'
 
 const app = express()
 app.set('trust proxy', true)
@@ -23,6 +24,7 @@ app.use(currentUser)
 app.use(createTicketRouter)
 app.use(showTicketRouter)
 app.use(indexTicketRouter)
+app.use(updateTicketRouter)
 
 // use "all" -- includes get, post, etc.
 app.all('*', async (req, res, next) => {
