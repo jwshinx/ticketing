d383774ea42ababdef80b78a631d65a36e86cd1d
Joel Shin <joel@fluxxlabs.com>
2022-06-02 20:38:47 -0700

    tickets: index.ts - fetch all tickets

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -7,6 +7,7 @@ import { errorHandler, NotFoundError, currentUser } from '@jslamela/common'
 
 import { createTicketRouter } from './routes/new'
 import { showTicketRouter } from './routes/show'
+import { indexTicketRouter } from './routes/index'
 
 const app = express()
 app.set('trust proxy', true)
@@ -21,6 +22,7 @@ app.use(
 app.use(currentUser)
 app.use(createTicketRouter)
 app.use(showTicketRouter)
+app.use(indexTicketRouter)
 
 // use "all" -- includes get, post, etc.
 app.all('*', async (req, res, next) => {
---
+++ tickets/src/routes/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import { Ticket } from '../models/ticket';
+// import { NotFoundError } from '@jslamela/common';
+
+const router = express.Router();
+
+router.get('/api/tickets', async (req: Request, res: Response) => {
+  // list out only tickets which havent been purchased
+  // const tickets = await Ticket.find({
+  //   orderId: undefined
+  // });
+  const tickets = await Ticket.find({})
+
+  res.send(tickets);
+});
+
+export { router as indexTicketRouter };
