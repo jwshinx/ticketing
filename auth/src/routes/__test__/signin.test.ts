f11b5149de21c926d242aa033b63bf0ec54bf013
Joel Shin <joel@fluxxlabs.com>
2022-06-14 00:06:40 -0700

    testing: auth signin and signout tests

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,47 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns a 401 on email which does not exist', async () => {
+  return request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(400);
+});
+
+it('returns a 201 and a valid cookie on successful signin', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+  const res = await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(200);
+  expect(res.get('Set-Cookie')).toBeDefined();
+});
+
+it('returns a 400 on failed signin', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'badbadbad'
+    })
+    .expect(400);
+});
