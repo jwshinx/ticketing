f9b7bec38638b755ef08c3e7925decee48019f66
Joel Shin <joel@fluxxlabs.com>
2022-06-13 17:19:06 -0700

    testing: rfr auth/src/index.ts to app.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,5 @@
-import express from 'express'
-import 'express-async-errors'
-import { json } from 'body-parser'
 import mongoose from 'mongoose'
-import cookieSession from 'cookie-session'
-import { errorHandler, NotFoundError } from '@jslamela/common'
-
-import { currentUserRouter } from './routes/current-user'
-import { signinRouter } from './routes/signin'
-import { signoutRouter } from './routes/signout'
-import { signupRouter } from './routes/signup'
-
-const app = express()
-app.set('trust proxy', true)
-app.use(json())
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true
-  })
-)
-
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signupRouter)
-app.use(signoutRouter)
-
-// use "all" -- includes get, post, etc.
-app.all('*', async (req, res, next) => {
-  // next(new NotFoundError())
-  throw new NotFoundError()
-})
-
-app.use(errorHandler)
+import { app } from './app'
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
