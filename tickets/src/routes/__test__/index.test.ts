2bf4535301e73e3aa919f261a3b3687e47e22c4d
Joel Shin <joel@fluxxlabs.com>
2022-06-16 12:20:42 -0700

    testing: tickets - index.test.ts passes with mocked natsWrapper

--- tickets/src/routes/__test__/index.test.ts
+++ tickets/src/routes/__test__/index.test.ts
@@ -2,6 +2,8 @@ import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
 
+jest.mock('../../nats-wrapper')
+
 const createTicket = () => {
   return request(app)
     .post('/api/tickets')
@@ -13,7 +15,7 @@ const createTicket = () => {
 }
 
 // 277
-xit('can fetch a list of tickets', async () => {
+it('can fetch a list of tickets', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
 
   await createTicket();
@@ -26,35 +28,34 @@ xit('can fetch a list of tickets', async () => {
   expect(resp.body.length).toEqual(2);
 });
 
+it('returns a 404 if the ticket is not found', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
 
-// it('returns a 404 if the ticket is not found', async () => {
-//   const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app)
+    .get(`/api/tickets/${id}`)
+    .send()
+    .expect(404);
+});
 
-//   await request(app)
-//     .get(`/api/tickets/${id}`)
-//     .send()
-//     .expect(404);
-// });
+it('returns the ticket if the ticket is found', async () => {
+  const title = 'band';
+  const price = 200;
 
-// it('returns the ticket if the ticket is found', async () => {
-//   const title = 'band';
-//   const price = 200;
-
-//   const resp = await request(app)
-//     .post('/api/tickets')
-//     .set('Cookie', global.signin())
-//     .send({
-//       title, price
-//     })
-//     .expect(201);
+  const resp = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title, price
+    })
+    .expect(201);
   
-//   // console.log("+++> 1", resp.body);
-//   const ticketResp = await request(app)
-//     .get(`/api/tickets/${resp.body.id}`)
-//     .send()
-//     .expect(200);
+  // console.log("+++> 1", resp.body);
+  const ticketResp = await request(app)
+    .get(`/api/tickets/${resp.body.id}`)
+    .send()
+    .expect(200);
   
-//   // console.log("+++> 2", ticketResp.body);
-//   expect(ticketResp.body.title).toEqual(title);
-//   expect(ticketResp.body.price).toEqual(price);
-// });
+  // console.log("+++> 2", ticketResp.body);
+  expect(ticketResp.body.title).toEqual(title);
+  expect(ticketResp.body.price).toEqual(price);
+});
