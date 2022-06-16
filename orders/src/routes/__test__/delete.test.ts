0445916cad29b7b5b643fc9dd342d1796d6c0b1b
Joel Shin <joel@fluxxlabs.com>
2022-06-16 13:52:32 -0700

    testing: order service delete.test.ts

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+import { Order, OrderStatus } from '../../models/order';
+import { natsWrapper } from '../../nats-wrapper';
+
+it('marks an order as cancelled', async () => {
+  const t = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: 'play',
+    price: 30
+  });
+  await t.save();
+
+  const u = global.signin();
+  
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', u)
+    .send({ ticketId: t.id })
+    .expect(201);
+
+  const { body: fetchedOrder } = await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', u)
+    .send()
+    .expect(204);
+
+  const updatedOrder = await Order.findById(order.id);
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it.todo('emits an order cancelled event')
+
+// it('emits an order cancelled event', async () => {
+//   const t = Ticket.build({
+//     id: new mongoose.Types.ObjectId().toHexString(),
+//     title: 'play',
+//     price: 30
+//   });
+//   await t.save();
+
+//   const u = global.signin();
+  
+//   const { body: order } = await request(app)
+//     .post('/api/orders')
+//     .set('Cookie', u)
+//     .send({ ticketId: t.id })
+//     .expect(201);
+
+//   const { body: fetchedOrder } = await request(app)
+//     .delete(`/api/orders/${order.id}`)
+//     .set('Cookie', u)
+//     .send()
+//     .expect(204);
+
+//   expect(natsWrapper.client.publish).toHaveBeenCalled();
+// });
