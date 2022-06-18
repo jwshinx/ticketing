17af1d827029d997c08eba650d2a26f9d0448be1
Joel Shin <joel@fluxxlabs.com>
2022-06-18 16:39:09 -0700

    css of signup, signin pages

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -24,28 +24,30 @@ const Signup = () => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
+    <div className='mt-3'>
       <h1>Sign In</h1>
-      <div className="form-group">
-        <label>Email Address</label>
-        <input
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          className="form-control"
-        />
-      </div>
-      <div className="form-group">
-        <label>Password</label>
-        <input
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          type="password"
-          className="form-control"
-        />
-      </div>
-      {errors}
-      <button className="btn btn-primary">Sign In</button>
-    </form>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Email Address</label>
+          <input
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group mb-3">
+          <label>Password</label>
+          <input
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            type="password"
+            className="form-control"
+          />
+        </div>
+        {errors}
+        <button className="btn btn-primary">Sign In</button>
+      </form>
+    </div>
   );
 };
 
