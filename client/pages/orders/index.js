314146538ed3d9c3a5bfc8ed280eae9f940f185e
Joel Shin <joel@fluxxlabs.com>
2022-06-18 14:59:34 -0700

    test: updated startup messages

--- client/pages/orders/index.js
+++ client/pages/orders/index.js
@@ -16,7 +16,7 @@ const OrderIndex = ({ orders }) => {
       <table className="table">
         <thead>
           <tr>
-            <th>Ticket</th>
+            <th>Ticket 256</th>
             <th>Price</th>
             <th>Status</th>
           </tr>
