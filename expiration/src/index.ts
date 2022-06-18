56cb2d4291eba341b6c48aca19610330d9ae0c22
joel shin <joel@fluxxlabs.com>
2022-06-18 15:02:35 -0700

    test: updated startup messages (#7)

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -29,6 +29,7 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 // app.use(errorHandler)
 
 const start = async () => {
+  console.log('+++> expiration 256')
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error('NATS_CLIENT_ID must be defined')
   }
--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,7 +8,7 @@ import { ExpirationCompleteListener } from './events/listeners/expiration-comple
 import { PaymentCreatedListener } from './events/listeners/payment-created-listener'
 
 const start = async () => {
-  console.log('xxx starting xxx 437pm')
+  console.log('xxx starting xxx 256')
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined')
   }
