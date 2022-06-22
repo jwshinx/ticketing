5eb3fc128b80fe307724241a9577a6be8d3f8972
joel shin <joel@fluxxlabs.com>
2022-06-22 12:01:13 -0700

    created dev, prod versions of client-depl ensuring baseUrl gets set differently (#19)

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,12 +1,21 @@
 import axios from 'axios';
 
 const BuildClient = ({ req }) => {
+  console.log("+++> client buildClient 6/22 1145am process.env.JOEL_ENVIRONMENT:", process.env.JOEL_ENVIRONMENT);
+
+  let baseUrlValue = 'http://www.customguitarsandmore.com'
+  if (process.env.JOEL_ENVIRONMENT === 'development') {
+    baseUrlValue = 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local'
+    console.log(`+++> client buildClient 6/22 1156am baseUrl set to ${baseUrlValue}`);
+  }
+
   if (typeof window === 'undefined') {
     console.log("+++> buildClient for server env 1235pm");
 
     return axios.create({
       // baseURL: 'http://www.customguitarsandmore.com',
-      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      // baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: baseUrlValue,
       headers: req.headers
     });
   } else {
